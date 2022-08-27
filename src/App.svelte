a50f2b338332fa67a418675962b25e0698ca4796
Arturo Coronel <contact@yayu.dev>
2022-08-26 19:21:22 -0700

    WIP

---
+++ src/App.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+  import copyIcon from './assets/icons/copy.svg';
+  import Checkbox from './components/checkbox.svelte';
+
+  let password = "P4$5w0rD!"
+  let options = {
+    uppercase: true,
+    lowercase: true,
+    numbers: true,
+    symbol: true,
+  };
+</script>
+
+
+<div class="container">
+  <h1 class="title">Password generator</h1>
+
+  <div class="content-box">
+    <div class="password">
+      <p class="password-text">{password}</p>
+      <img class="password-copy-icon" src={copyIcon} alt="Copy password" />
+    </div>
+  </div>
+
+  <div class="content-box">
+<!--    <div>-->
+<!--      <span>Character length</span>-->
+<!--      <span>9</span>-->
+<!--    </div>-->
+    <div class="options">
+      <Checkbox bind:checked={options.uppercase} label="Include uppercase letters" />
+      <Checkbox bind:checked={options.lowercase} label="Include lowercase letters" />
+      <Checkbox bind:checked={options.numbers} label="Include number" />
+      <Checkbox bind:checked={options.symbol} label="Include special characters" />
+    </div>
+
+    {JSON.stringify(options)}
+  </div>
+</div>
+
+
+<style lang="scss">
+  .container {
+    margin: 0 auto;
+    padding: 1rem;
+    width: 95%;
+    max-width: 780px;
+    text-align: center;
+  }
+
+  .title {
+    letter-spacing: 2px;
+  }
+
+  .content-box {
+    background: rgba(255, 255, 255, 0.3);
+    border-radius: var(--border-radius);
+    margin: 1.5rem 0;
+    padding: 1rem;
+  }
+
+  .options {
+    margin: 0 1rem;
+  }
+
+  .password {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+
+    .password-copy-icon {
+      filter: opacity(50%);
+    }
+
+    &:hover .password-copy-icon {
+      filter: opacity(100%);
+    }
+  }
+
+  .password-text {
+    font-size: 2rem;
+    margin: 0;
+  }
+</style>
---
+++ src/components/checkbox.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+    export let checked: boolean = false;
+    export let label: string = "";
+
+    $: checkboxId = `checkbox-${label}`;
+
+    function toggleCheckbox() {
+        checked = !checked;
+    }
+</script>
+
+
+    <div class="checkbox-row">
+        <div role="checkbox" class="checkbox-outside" on:click={toggleCheckbox}>
+            <div class="checkbox-fill" class:active={checked}></div>
+        </div>
+
+        <label for={checkboxId} class="checkbox-label">{label}</label>
+
+        <input
+            type="checkbox"
+            id={checkboxId}
+            bind:checked={checked}
+            hidden
+        />
+    </div>
+
+<style lang="scss">
+    .checkbox-row {
+      display: flex;
+      font-size: 1.2rem;
+      margin: 1.5rem 0;
+      align-items: center;
+      width: 100%;
+    }
+
+    .checkbox-label {
+      margin-left: 1.25rem;
+    }
+
+    .checkbox-outside {
+      cursor: pointer;
+      user-select: none;
+      height: 30px;
+      width: 30px;
+      display: flex;
+      place-items: center;
+      background: var(--inactive-color);
+      border-radius: var(--border-radius);
+    }
+
+    .checkbox-fill {
+      background: #91CBF1;
+      height: 24px;
+      width: 24px;
+      margin: 3px;
+      border-radius: 2px;
+
+      &.active {
+        background: var(--main-text-color);
+      }
+    }
+</style>
